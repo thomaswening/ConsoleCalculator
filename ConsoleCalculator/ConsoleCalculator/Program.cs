957c883ab7b00870d136bfa472a1b9a3eb9ad16b
thomaswening <thomaswening94@gmail.com>
2022-06-27 21:21:55 +0200

    add IO logic to class Program

--- ConsoleCalculator/ConsoleCalculator/Program.cs
+++ ConsoleCalculator/ConsoleCalculator/Program.cs
@@ -4,11 +4,50 @@
     {
         static void Main()
         {
-            Console.Write("Please, enter a mathematical expression to evaluate.\n>> ");
-            string input = "100. - (2.5 - .5 * ((-1)*(-40.) + 10))"; // Console.ReadLine();
-            Calculation calc = new(input);
+            bool repeat = false;
 
-            Console.WriteLine($"\n{new Calculation(input).Evaluate()}");
+            Console.Write("Please, enter a mathematical expression to evaluate.\n>> ");
+            do
+            {
+                try
+                {
+                    string? rawExpression = Console.ReadLine();
+                    Console.WriteLine("\n");
+                    if (string.IsNullOrEmpty(rawExpression)) throw new ArgumentException("Please enter something.");
+
+                    Console.WriteLine($"\n>> {new Calculation(rawExpression).Evaluate()}");
+
+                    bool isYOrN;
+                    Console.Write("Do you want to evaluate another expression? (y/n)\n>> ");
+                    do
+                    {
+                        try
+                        {
+                            string? input = Console.ReadLine();
+                            Console.WriteLine("\n");
+
+                            isYOrN = true;
+                            if (string.IsNullOrEmpty(input)) throw new ArgumentException("Please enter something.");
+                            else if (input == "y") repeat = true;
+                            else if (input == "n") repeat = false;
+                            else throw new ArgumentException("Please enter either 'y' or 'n'.");
+                        }
+                        catch (Exception e)
+                        {
+                            isYOrN = false;
+                            Console.WriteLine(e.Message);
+                        }
+
+                    } while (!isYOrN);
+
+                }
+                catch (Exception e)
+                {
+                    repeat = true;
+                    Console.WriteLine(e.Message);
+                }
+
+            } while (repeat);
 
         }
     }
